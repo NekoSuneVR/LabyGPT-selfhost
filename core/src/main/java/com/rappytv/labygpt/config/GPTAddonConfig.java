0a79161e52fb27485e6fcd4785ac07ddcc273ff2
RappyTV <rappytv@rappytv.com>
2023-06-25 18:49:43 +0200

    Add sprites

--- core/src/main/java/com/rappytv/labygpt/config/GPTAddonConfig.java
+++ core/src/main/java/com/rappytv/labygpt/config/GPTAddonConfig.java
@@ -3,16 +3,23 @@ package com.rappytv.labygpt.config;
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @ConfigName("settings")
+@SpriteTexture("settings")
 public class GPTAddonConfig extends AddonConfig {
 
     @SwitchSetting
+    @SpriteSlot(size = 32)
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @SpriteSlot(size = 32, x = 1)
     private final OpenAISubConfig openAI = new OpenAISubConfig();
+    @SpriteSlot(size = 32, x = 1)
     private final GPTSubConfig gpt = new GPTSubConfig();
     @SwitchSetting
+    @SpriteSlot(size = 32, x = 2, y = 1)
     private final ConfigProperty<Boolean> saveHistory = new ConfigProperty<>(true);
 
     @Override
